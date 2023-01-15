f829c40b1aff1801a4f2b8dceced6839d069fd20
Samir Bagamaev <05ru4315@gmail.com>
2023-01-15 17:48:30 +0300

    work_27

---
+++ WORK_27/Program.cs
@@ -0,0 +1,26 @@
+﻿// //  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("ВВЕДИТЕ ЧИСЛО: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber (int number)
+{
+   int result = 0; // конечный результат (сумма цифр заданного числа)
+   int Digit = 0; 
+   int product = 0; // произведение ( произдение цифр заданного числа )
+
+   for(int i = 0; i <= numberN; i++) // забыл добавить N к слову number
+    { 
+    Digit = number % 10;
+    result = result + Digit;
+    product = product * Digit;
+    number = number / 10;
+    }
+   return result;
+}
+int sumNumber = SumNumber(numberN);
+Console.WriteLine(sumNumber);
